e2c9ce45d2be6bfb21cbc2e2cbb661194ac41b84
4j0 <4j0@4j0deMacBook-Pro.local>
2017-04-20 13:37:29 +0800

    Remove api

--- api/funcs.py
+++
@@ -1,5 +0,0 @@
-def jsonEncoder(obj):
-    if hasattr(obj, '__table__'):
-        return {c.name: getattr(obj, c.name) for c in obj.__table__.columns}
-    return json.JSONEncoder.default(self, obj)
-
--- api/wsgi_conf.wsgi
+++
@@ -1,5 +0,0 @@
-import sys, logging
-sys.path.insert(0, '/var/www/Asset-Management/api')
-logging.basicConfig(stream=sys.stderr)
-from app import app as application
-#import hello
