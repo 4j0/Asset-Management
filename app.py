dcbbb57326831643cd106149e19ec32252287624
4j0 <4j0@4j0deMacBook-Pro.local>
2017-07-21 14:36:06 +0800

    Add the sort function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 from flask import Flask
 from db_conf import DB_CONFIG
 from model import db
@@ -6,6 +7,9 @@ from modules.asset import AssetManagement
 from modules.login import Login
 from modules.user import UserManagement
 from modules.role import RoleManagement
+
+debug = False
+if '--debug' in sys.argv: debug = True
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s/%s' % (\
@@ -25,5 +29,5 @@ def initialize_database():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug = debug)
 
